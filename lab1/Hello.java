ab1282594fadb3de9a51b2471e9664d19e0fc7e6
Evan Aurthur Simmons <easimmon@unix2.ic.ucsc.edu>
2011-09-27 10:36:36 -0700

    created 1st lab

---
+++ lab1/Hello.java
@@ -0,0 +1,14 @@
+/* Hello.java
+* Purpose: To print hello.
+* Author: Evan Simmons
+*/
+
+class Hello {
+
+    public static void main (String[] args) { 
+
+    System.out.println("The CS12A forum url is: http://forums.soe.ucsc.edu/viewforum.php?f=39 ");
+
+    }
+
+}
