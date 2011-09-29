b61ea3b2e2fb12321ee25cd42af5012868633a35
evan <esims89@gmail.com>
2011-09-29 13:07:12 -0700

    added test cases

---
+++ proj1/test/MortgageTests.java
@@ -0,0 +1,20 @@
+import org.junit.* ;
+import static org.junit.Assert.* ;
+
+public class MortgageTest {
+  
+  @before
+  public void setUp() {
+    
+  }
+  
+  @test
+  public void calcApr() {
+    
+  }
+  
+  @test
+  public void calcPayment() {
+    
+  }
+}
