d3ef34bc62860b344f2b37fe82c3427c9ba992d0
evan <esims89@gmail.com>
2011-09-29 14:44:38 -0700

    Added tests

--- proj1/Mortgage.java
+++ proj1/Mortgage.java
@@ -1,30 +1,38 @@
 import java.util.*;
 
 class Mortgage {
-  private static int amount, duration;
-  private static double monthlyRate;
+  private int amount, duration;
+  private double monthlyRate;
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in); 
     System.out.println("What is your loan amount?");
-    amount = scan.nextInt();
+    int amnt = scan.nextInt();
     System.out.println("What is the interest rate?");
-    monthlyRate = scan.nextDouble() / 1200;
+    double rate = scan.nextDouble();
     System.out.println("How many years is the loan for?");
-    duration = scan.nextInt();
+    int dura = scan.nextInt();
+    
+    Mortgage mort = new Mortgage(amnt, rate, dura);
     
     System.out.println( "Your APR is:" );
-    System.out.printf(  "%.2f percent \n", calcApr() );
+    System.out.printf(  "%.2f percent \n", mort.calcApr() );
     System.out.println( "Your monthly mortgage payment is:" );
-    System.out.printf(  "$%.2f \n", calcPayment() );
+    System.out.printf(  "$%.2f \n", mort.calcPayment() );
+  }
+  
+  public Mortgage(int amnt, double rate, int dura) {
+    amount = amnt;
+    monthlyRate = rate / 1200;
+    duration = dura;
   }
 
-  private static double calcApr() {
+  public double calcApr() {
     double apr = Math.pow((1 + monthlyRate), 12) - 1;
     return apr * 100;  //make into a percent
   }
 
-  private static double calcPayment() {
+  public double calcPayment() {
     double top = (amount * monthlyRate);
     double bottom =(1- 1/ Math.pow((1 + monthlyRate), duration * 12));
     return top / bottom;
---
+++ proj1/MortgageTests.java
@@ -0,0 +1,25 @@
+import org.junit.* ;
+import static org.junit.Assert.* ;
+import java.text.* ;
+
+public class MortgageTests {
+public Mortgage mort;
+public DecimalFormat df, df2;
+  
+  @Before
+  public void setUp() {
+    df = new DecimalFormat("#.##");
+    df2 = new DecimalFormat("####.##");
+    mort = new Mortgage(500000, 4.5, 30);
+  }
+  
+  @Test
+  public void calcApr() {
+    assertEquals( df.format(mort.calcApr()), df.format(4.59) );
+  }
+  
+  @Test
+  public void calcPayment() {
+    assertEquals( df2.format(mort.calcPayment()), df2.format(2533.43) );
+  }
+}
