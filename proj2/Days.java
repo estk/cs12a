b3a6ca623c4858396f8ffd59f678de3b3931af8f
evan <esims89@gmail.com>
2011-10-06 12:47:13 -0700

    Cleaned up monthToDays

--- proj2/Days.java
+++ proj2/Days.java
@@ -127,33 +127,11 @@ public class Days {
    */
 
   public static int monthToDays(int month) {
-    switch (month) {
-      case 1:
-        return 0;
-      case 2:
-        return 31;
-      case 3:
-        return 59;
-      case 4:
-        return 90;
-      case 5:
-        return 120;
-      case 6:
-        return 151;
-      case 7:
-        return 118;
-      case 8:
-        return 212;
-      case 9:
-        return 243;
-      case 10:
-        return 273;
-      case 11:
-        return 304;
-      case 12:
-        return 334;
-      default: return 0;
+    int i, res = 0;
+    for (i = month - 1 ; i > 0 ; i--) {
+      res += daysInMonth(i);
     }
+    return res;
   }
   
   /**
