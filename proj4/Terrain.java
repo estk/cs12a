ea8e700f809003d55219d7ff5ba7c1aaba38a37f
evan <esims89@gmail.com>
2011-11-04 17:19:39 -0700

    made PmgImageInfo non-static, added Exceptions

--- proj4/Terrain.java
+++ proj4/Terrain.java
@@ -12,30 +12,27 @@ public class Terrain {
         System.out.println("Bye.");
     }
 
-    static int pgmWidth() {
-        return pgmInf.imageData.length;
-    }
-
-    static int pgmHeight() {
-        return pgmInf.imageData[0].length;
-    }
-
     static void loadPgm() {
         String file;
-        // Prompt for pgm
-        System.out.print("Enter terrain file: "); file = scan.nextLine();
-        // Read pgm
-        pgmInf = PgmImageInfo.readPgmFile( file );
-        if (null == pgmInf) loadPgm();
-        // More Interaction
-        System.out.printf("Read terrain file: %d by %d, ",  pgmWidth(), pgmHeight() );
-        System.out.printf("height ranges from %d to %d.\n\n", 0, pgmInf.maxValue);
+        try {
+            // Prompt for pgm
+            System.out.print("Enter terrain file: "); file = scan.nextLine();
+            // Read pgm
+            pgmInf = new PgmImageInfo( file );
+            // More Interaction
+            System.out.printf("Read terrain file: %d by %d, ",  pgmInf.width, pgmInf.height );
+            System.out.printf("height ranges from %d to %d.\n", 0, pgmInf.maxValue);
+        }
+        catch (Exception e) {
+            System.out.println("readPgmFile: failed to read file!  Error!: " + e.getLocalizedMessage());
+            loadPgm();
+        }
     }
 
     static void actionPrompt() {
         prompt:
         while (true) {
-            System.out.println("What do you want to do: (S)ave, (F)ill, (L)ine, (V)isible, (Q)uit: ");
+            System.out.println("\nWhat do you want to do: (S)ave, (F)ill, (L)ine, (V)isible, (Q)uit: ");
             char action = scan.nextLine().charAt(0);
             switch (action) {
                 case 'Q':
@@ -68,12 +65,13 @@ public class Terrain {
                 default: save();
             }
         }
-        saveFile(fileName);
+        pgmInf.savePgmFileAs(fileName);
+        System.out.println("Terrain file saved.");
     }
 
     static void fill() {
         int x, y, height;
-        System.out.println("Enter reference point: ");
+        System.out.print("Enter reference point: ");
         x = scan.nextInt(); y = scan.nextInt();
         // calcs
         if ( pointInPgm( x, y ) ) {
@@ -88,7 +86,16 @@ public class Terrain {
     }
 
     static void line() {
-        
+        int x1, x2, y1, y2;
+        System.out.print("Enter point 1: ");
+        x1 = scan.nextInt(); y1 = scan.nextInt();
+        System.out.print("Enter point 2: ");
+        x2 = scan.nextInt(); y2 = scan.nextInt();
+
+        if (lineBetween(x1, y1, x2, y2))
+            System.out.println("Point 2 is visible from point 1.");
+        else
+            System.out.println("Point 2 is not visible from point 1.");
     }
 
     static void visible() {
@@ -99,29 +106,30 @@ public class Terrain {
        return true; 
     }
 
-    static void saveFile(String fileName) {
-        
+    static Boolean lineBetween(int x1, int y1, int x2, int y2) {
+        return true; 
     }
 
     static Boolean pointInPgm(int x, int y) {
-        return (x >= 0 ) && (y >= 0) && (x <= pgmWidth()) && (y <= pgmHeight());
+        return (x >= 0 ) && (y >= 0) && (x <= pgmInf.width) && (y <= pgmInf.height);
     }
 
     static int getPixValue(int x, int y) {
-        return pgmInf.imageData[x][y];
+        return pgmInf.img[x][y];
     }
 
     static void fillGt(int height) {
         int pixel;
-        for (int x=0 ; x <= pgmWidth() ; x++) {
-            for (int y=0 ; y <= pgmWidth() ; y++) {
-                pixel = pgmInf.imageData[x][y]; 
+        for (int x=0 ; x <= pgmInf.width ; x++) {
+            for (int y=0 ; y <= pgmInf.height ; y++) {
+                pixel = pgmInf.img[x][y]; 
                 if ( pixel < height)
-                    pgmInf.imageData[x][y] = 0;
-                pgmInf.imageData[x][y] = pgmInf.maxValue / 2;
+                    pgmInf.img[x][y] = 0;
+                pgmInf.img[x][y] = pgmInf.maxValue / 2;
             }
         }
     }
+}
 
 
 
@@ -133,10 +141,10 @@ public class Terrain {
     /**
     * Inner class to wrap up the PGM file attributes into a single object for easier access
     */
-    private static class PgmImageInfo {
-            public ArrayList comments = new ArrayList();
-            public int maxValue = 0;
-            public int[][] imageData = null;
+class PgmImageInfo {
+        public ArrayList comments = new ArrayList();
+        public int maxValue = 0, width, height;
+        public int[][] img = null;
 
     /**
      * Creates a 2D array representing the PGM file given.
@@ -144,73 +152,96 @@ public class Terrain {
      * @param filename - the filename for the PGM file to read in
      * @return A PgmImageInfo object containing the representation of the PGM file
      */
-    public static PgmImageInfo readPgmFile(String filename) {
+    public PgmImageInfo(String filename)
+    throws DateInvalidException, IOException
+    {
+        BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
 
-            try {
-                    PgmImageInfo pgmImageInfo = new PgmImageInfo();
+        // read in the file type
+        String fileType = reader.readLine();
 
-                    BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
-
-                    // read in the file type
-                    String fileType = reader.readLine();
-
-                    if(fileType == null || !fileType.equalsIgnoreCase("P2")) {
-                            System.out.println("readPgmFile: Wrong file type!");
-                            return null;
-                    }
+        if(fileType == null || !fileType.equalsIgnoreCase("P2")) {
+                System.out.println("readPgmFile: Wrong file type!");
+                throw new DateInvalidException("readPgmFile: Wrong file type!");
+        }
 
 
-                    // read in the dimensions of the image
-                    String imageSizeString = reader.readLine();
+        // read in the dimensions of the image
+        String imageSizeString = reader.readLine();
 
-                    if(imageSizeString == null) {
-                            System.out.println("readPgmFile: No Image Dimensions!");
-                            return null;
-                    }
+        if(imageSizeString == null) {
+                System.out.println("readPgmFile: No Image Dimensions!");
+                throw new DateInvalidException("readPgmFile: No Image Dimensions!");
+        }
 
-                    // bypass any comments
-                    while(imageSizeString.startsWith("#")) {
-                            pgmImageInfo.comments.add(imageSizeString);
-                            imageSizeString = reader.readLine();
-                    }
+        // bypass any comments
+        while(imageSizeString.startsWith("#")) {
+                comments.add(imageSizeString);
+                imageSizeString = reader.readLine();
+        }
 
-                    // done with comments, so image size string is actually the image size
+        // done with comments, so image size string is actually the image size
 
-                    // split the string up to get the dimensions
-                    String[] tokens = imageSizeString.split("\\s+");
+        // split the string up to get the dimensions
+        String[] tokens = imageSizeString.split("\\s+");
 
-                    if(tokens.length != 2) {
-                            System.out.println("readPgmFile: Incorrrect Image Dimension Definitions!");
-                            return null;
-                    }
+        if(tokens.length != 2) {
+                System.out.println("readPgmFile: Incorrrect Image Dimension Definitions!");
+                throw new DateInvalidException("readPgmFile: Incorrrect Image Dimension Definitions!");
+        }
 
-                    // create and populate the image array
-                    int[][] pgmArray = new int[Integer.parseInt(tokens[1])][Integer.parseInt(tokens[0])];
+        // create and populate the image array
+        int[][] pgmArray = new int[Integer.parseInt(tokens[1])][Integer.parseInt(tokens[0])];
 
-                    // get the max value
-                    pgmImageInfo.maxValue = Integer.parseInt(reader.readLine());
+        // get the max value
+        maxValue = Integer.parseInt(reader.readLine());
 
-                    String imageRow = reader.readLine();
-                    int row = 0;
-                    while(imageRow != null) {
+        String imageRow = reader.readLine();
+        int row = 0;
+        while(imageRow != null) {
 
-                            tokens = imageRow.split("\\s+");
+                tokens = imageRow.split("\\s+");
 
-                            for(int rowIndex = 0; rowIndex < tokens.length; ++rowIndex)
-                                    pgmArray[row][rowIndex] = Integer.parseInt(tokens[rowIndex]);
+                for(int rowIndex = 0; rowIndex < tokens.length; ++rowIndex)
+                        pgmArray[row][rowIndex] = Integer.parseInt(tokens[rowIndex]);
 
-                            imageRow = reader.readLine();
-                            ++row;
-                    }
+                imageRow = reader.readLine();
+                ++row;
+        }
 
-                    pgmImageInfo.imageData = pgmArray;
+        img = pgmArray;
 
-                    return pgmImageInfo;
-            }
-            catch(IOException e) {
-                    System.out.println("readPgmFile: failed to read file!  Error: " + e.getLocalizedMessage());
-                    return null;
-            }
+        // set width and height
+        width = img.length;
+        height = img[0].length;
+}
+    public void savePgmFileAs(String filename) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File(filename)));
+            // write filetype
+            writer.write("P2\n");
+            // write dimensions
+            writer.write(width + " " + height + "/n");
+            // write image
+            writer.write( toString() );
+            writer.close();
+        }
+        catch (IOException e) {
+            System.out.println("readPgmFile: failed to read file!  Error: " + e.getLocalizedMessage());
+        }
     }
+
+    public String toString() {
+        String res = "";
+        for (int i=0 ; i <= this.width ; i++) {
+            for (int j=0 ; j <= this.height ; i++) {
+                res += this.img[i][j] + " ";
+            }
+            res += "\n";
+        }
+        return res;
     }
 }
+class DateInvalidException extends Exception {
+    public DateInvalidException(String msg) { super(msg); }
+  }
